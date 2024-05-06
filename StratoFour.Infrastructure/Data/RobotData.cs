3d95028fbcee8e76ffe586654c06ec372b0fe6a2
shannonwuerzer <96075376+shannonwuerzer@users.noreply.github.com>
2024-05-06 16:39:54 +0200

    Add robot crud

---
+++ StratoFour.Infrastructure/Data/RobotData.cs
@@ -0,0 +1,35 @@
+﻿using StratoFour.Infrastructure.DbAccess;
+using StratoFour.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StratoFour.Infrastructure.Data;
+
+public class RobotData : IRobotData
+{
+    private readonly ISqlDataAccess _db;
+
+    public RobotData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<RobotModel>> GetRobots() =>
+        _db.LoadData<RobotModel, dynamic>("dbo.spUser_GetAll", new { });
+
+    public async Task<RobotModel> GetRobots(int id)
+    {
+        var results = await _db.LoadData<RobotModel, dynamic>("dbo.spRobot_Get", new { Id = id });
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertRobots(RobotModel robot) => _db.SaveData("dbo.spRobot_Insert", new { robot.RobotName, robot.RobotStatus });
+
+
+    public Task UpdateUser(RobotModel robot) => _db.SaveData("dbo.spRobot_Update", robot);
+
+    public Task DeleteUser(int id) => _db.SaveData("dbo.spRobot_Delete", new { Id = id });
+}
---
+++ StratoFour.Infrastructure/Models/RobotModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StratoFour.Infrastructure.Models;
+
+public class RobotModel
+{
+    public string RobotName { get; set; }
+    public string RobotStatus { get; set; }
+}
