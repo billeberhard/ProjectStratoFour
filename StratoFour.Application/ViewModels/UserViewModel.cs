045c36b86b17b412fba1b52dd4e1e50be041f97b
shannonwuerzer <shannon.wuerzer@bluewin.ch>
2024-05-13 11:33:20 +0200

    Add user view model

---
+++ StratoFour.Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StratoFour.Application.ViewModels;
+
+internal class UserViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "E-Mail")]
+    public string Email { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "User Name")]
+    public string UserName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
